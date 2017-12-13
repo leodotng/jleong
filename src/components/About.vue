df5d6c21e813e7508fa434e9bb35486998fff129
Josh <leong.jo@gmail.com>
2017-12-13 15:01:35 -0600

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+<div id="about">
+  About Me: Josh Leong
+  </div>
+  <div id="details">
+    I am a Full Stack Engineer Student at Galvanize's gSchool in Denver, CO.
+    
+  </div>
+  <div>
+    <a href="/" target="_blank">Back</a></li>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+<style>
+#about {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 80px;
+  margin-top: 60px;
+}
+#details {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 30px;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg2 }}</h1>
+    <img src="https://s3-us-west-2.amazonaws.com/quilt-production/logo/2017/10/Hghb4ApkwpAvSF7MJI.png">
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://talent.galvanize.com/students/1973" target="_blank">Galvanize Talent Profile</a></li>
+      <li><a href="https://www.linkedin.com/in/joshleong/" target="_blank">Linkedin</a></li>
+      <li><a href="https://www.twitter.com/leodotng" target="_blank">Twitter</a></li>
+      <li><a href="https://www.facebook.com/leodotng" target="_blank">Facebook</a></li>
+      <li><a href="https://www.github.com/leodotng" target="_blank">Github</a></li>
+      <br>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Josh Leong',
+      msg2: 'http://josh.leo.ng'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+img {
+    border-radius: 50%;
+}
+</style>
